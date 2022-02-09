142554d155b79b9e5094c1c499b0129869d5abbc
winonafeminine <winonafeminine.dev2021@gmail.com>
2022-02-09 04:27:58 +0000

    update scripts

--- scripts/stop-dc.sh
+++ scripts/stop-dc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # force remove the containers
+# for avoiding the containers to start at startup
 docker rm -f internship-demo_devcontainer_dotnet_1 \
     internship-demo_devcontainer_mariadb_1 \
     internship-demo_devcontainer_phpmyadmin_1 \
